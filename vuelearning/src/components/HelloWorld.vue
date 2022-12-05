58688aaf84336e4faaff6d7f7b1b03a8c53d828f
zlh <17629334818@qq.com>
2022-12-05 17:25:02 +0800

    购物车全选代码

--- vuelearning/src/components/HelloWorld.vue
+++ vuelearning/src/components/HelloWorld.vue
@@ -1,60 +1,105 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <!-- 购物车数据渲染 -->
+    <div v-for="(item, index) in arr" :key="index">
+      <p>
+        <input
+          type="checkbox"
+          v-model="item.ischeck"
+          @click="changeOne(index)"
+        />
+        {{ item.name }} -------价格：{{ item.price }}
+      </p>
+    </div>
+    <!-- 底部 -->
+    <div class="footer">
+      <input
+        type="checkbox"
+        v-model="checkAll"
+        @click="changeAlls(checkAll)"
+      />全选---
+    </div>
+    <div style="margin-top: 20px">总价：{{ allPrice }}</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      //模拟购物车数据arr
+      arr: [
+        {
+          ischeck: false, //默认为false
+          name: "篮球",
+          price: 98,
+        },
+        {
+          ischeck: false, //默认为false
+          name: "足球",
+          price: 10,
+        },
+        {
+          ischeck: false, //默认为false
+          name: "羽毛球",
+          price: 5,
+        },
+        {
+          ischeck: false, //默认为false
+          name: "排球",
+          price: 100,
+        },
+      ],
+      checkAll: false,
+      allPrice: 0,
+    };
+  },
+  methods: {
+    // 封装计算总价方法，在每次勾选单选框或者全选框时，便利arr数组里isCheck的值为true的集合，相加它们的价格，通过v-model计算总价
+    countPrice() {
+      let _price = 0;
+      this.arr.forEach((item) => {
+        if (item.ischeck) {
+          _price = _price + item.price;
+        }
+      });
+      this.allPrice = _price;
+    },
+    //关键步一，单选时，便利数组，使用findIndex方法，确定this.checkAll的值
+    all() {
+      let _index = this.arr.findIndex((item) => {
+        return !item.ischeck;
+      });
+      //findIndex会传入一个函数，return值为true的第一个元素的位置，
+      //如果没有符合条件的元素则返回-1
+      if (_index === -1) {
+        this.checkAll = true;
+      } else {
+        this.checkAll = false;
+      }
+      this.countPrice();
+    },
+    // 勾选全选框时，将this.checkAll的值赋给所有的单选项
+    changeAlls() {
+      let checkAllValue = !this.checkAll;
+      this.arr.forEach((item) => {
+        return (item.ischeck = checkAllValue);
+      });
+      this.countPrice();
+    },
+    changeOne(item) {
+      this.arr[item].ischeck = !this.arr[item].ischeck;
+      this.all();
+      this.countPrice();
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
